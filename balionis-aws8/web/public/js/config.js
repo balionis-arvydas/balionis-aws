b0ac3b76b6d9a7b502c050861483a24ca6a23484
unknown <arvydas@balionis.com>
2020-09-17 18:20:09 +0200

    JIRA-0000: added rest-api config

--- balionis-aws8/web/public/js/config.js
+++ balionis-aws8/web/public/js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-2_fxjx8gPKy', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6ijp32grl9fosof9p18mlg18tj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'eu-west-2' // e.g. us-east-2
+        userPoolId: 'eu-west-2_fxjx8gPKy',
+        userPoolClientId: '6ijp32grl9fosof9p18mlg18tj',
+        region: 'eu-west-2' 
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://oqyscbwdug.execute-api.eu-west-2.amazonaws.com/prod',
     }
 };
